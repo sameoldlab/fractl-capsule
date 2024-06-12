7fff5bfd8dd63bd20a96a93d722a3540a680ecb1
ibro <sameoldlab@proton.me>
2024-06-12 16:33:36 +0100

    add @fractl-ui/types

---
+++ src/lib/types.d.ts
@@ -0,0 +1,82 @@
+import type { MapStore } from 'nanostores'
+
+type Store<T extends object> = {
+	subscribe: MapStore<T>['subscribe']
+}
+
+export type AccountDataError = {
+	account: null
+	balance: null
+	nameService: {
+		name: null
+		avatar: null
+	}
+}
+
+export type AccountDataResponse = {
+	account: {
+		address: string
+		addresses: string[]
+	}
+	balance: {
+		value: string
+		symbol: string
+	}
+
+	nameService:
+		| {
+				name: null
+				avatar: null
+		  }
+		| {
+				name: string
+				avatar: string | null
+		  }
+}
+
+export type AccountData = AccountDataError | AccountDataResponse
+
+export type Connector<T = object> = {
+	name: string
+	type?: string
+	icon?: string
+} & T
+
+export type StateConnected<Connector> = {
+	activeRequest?: null
+	current: Connector
+	status: 'connected'
+}
+
+export type StateDisconnected<Connector> = {
+	activeRequest?: Connector | null
+	current: null
+	status: 'connecting' | 'disconnected' | 'reconnecting'
+}
+
+export type State<C> = StateConnected<C> | StateDisconnected<C>
+
+export type ConfigDisconnected<Connector> = {
+	state: Store<StateDisconnected<Connector>>
+	accountData: Store<AccountDataError>
+}
+
+export type ConfigConnected<C extends Connector> = {
+	state: Store<StateConnected<C>>
+	accountData: Store<AccountDataResponse>
+}
+
+export type Config<C extends Connector> = {
+	connectors: readonly C[]
+	connect: (connector: C) => Promise<unknown> /* fix later */
+	/**
+	 * Checks if a connector in the list is already connected
+	 * then sets the first one found as the current Connector
+	 * @param {C[]} connectors list of Connectors for the given library
+	 * @returns
+	 */
+	reconnect: (connectors: C[]) => Promise<void>
+	disconnect: (connector?: C, opts?: object) => Promise<void>
+} & (ConfigDisconnected<C> | ConfigConnected<C>)
+
+export {}
